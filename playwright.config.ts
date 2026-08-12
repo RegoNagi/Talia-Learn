f0ebd302631b0c05e3157e268abc6b32f3ab8959
RegoNagi <airego902@gmail.com>
2026-08-12 19:04:48 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   retries: 1,
   reporter: 'list',
   use: {
-    baseURL: process.env.E2E_BASE_URL || 'https://talia-learn-lms-git-main-prod-talia7.vercel.app',
+    baseURL: process.env.E2E_BASE_URL || 'https://talia-learn-lms.vercel.app',
     trace: 'on-first-retry',
   },
   projects: [
